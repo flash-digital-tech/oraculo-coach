b2d05963f43e7440dc05302c94a4b63ebf1e77e3
Flash Digital Tech <159139704+flash-digital-tech@users.noreply.github.com>
2024-12-03 19:31:00 -0300

    Update mestre.py
    Atualização: tokens 12000

--- views/mestre.py
+++ views/mestre.py
@@ -33,58 +33,6 @@ if replicate_api is None:
     st.warning('Um token de API é necessário para determinados recursos.', icon='⚠️')
 
 
-
-
-################################################# ENVIO DE E-MAIL ####################################################
-############################################# PARA CONFIRMAÇÃO DE DADOS ##############################################
-
-# Função para enviar o e-mail
-def enviar_email(destinatario, assunto, corpo):
-    remetente = "mensagem@flashdigital.tech"  # Insira seu endereço de e-mail
-    senha = "sua_senha"  # Insira sua senha de e-mail
-
-    msg = MIMEMultipart()
-    msg['From'] = remetente
-    msg['To'] = destinatario
-    msg['Subject'] = assunto
-    msg.attach(MIMEText(corpo, 'plain'))
-
-    try:
-        server = smtplib.SMTP('mail.flashdigital.tech', 587)
-        server.starttls()
-        server.login(remetente, senha)
-        server.sendmail(remetente, destinatario, msg.as_string())
-        server.quit()
-        st.success("E-mail enviado com sucesso!")
-    except Exception as e:
-        st.error(f"Erro ao enviar e-mail: {e}")
-
-    # Enviando o e-mail ao pressionar o botão de confirmação
-    if st.button("DADOS CONFIRMADO"):
-        # Obter os dados salvos em st.session_state
-        nome = st.session_state.user_data["name"]
-        whatsapp = st.session_state.user_data["whatsapp"]
-        email = st.session_state.user_data["email"]
-
-        # Construindo o corpo do e-mail
-        corpo_email = f"""
-        Olá {nome},
-
-        Segue a confirmação dos dados:
-        - Nome: {nome}
-        - WhatsApp: {whatsapp}
-        - E-mail: {email}
-        - Agendamento : {dias} e {turnos}
-
-        Obrigado pela confirmação!
-        """
-
-        # Enviando o e-mail
-        enviar_email(email, "Confirmação de dados", corpo_email)
-
-
-#######################################################################################################################
-
 def showMestre():
 
     def ler_arquivos_txt(pasta):
@@ -272,28 +220,6 @@ def showMestre():
     
     '''
 
-    # Função para gerar o PDF
-    def create_pdf(messages):
-        pdf = FPDF()
-        pdf.add_page()
-        pdf.set_font("Arial", size=12)
-
-        for message in messages:
-            role = message["role"].capitalize()
-            content = message["content"]
-            pdf.cell(200, 10, txt=f"{role}: {content}", ln=True)
-
-        return pdf.output(dest='S').encode('latin1')
-
-
-    # Função para gerar o Excel
-    def create_excel(messages):
-        df = pd.DataFrame(messages)
-        buffer = io.BytesIO()
-        df.to_excel(buffer, index=False)
-        buffer.seek(0)
-        return buffer.getvalue()
-
 
     # Set assistant icon to Snowflake logo
     icons = {"assistant": "./src/img/perfil-alan.jpg", "user": "./src/img/usuario.jpg"}
@@ -422,40 +348,7 @@ def showMestre():
         prompt.append("<|im_start|>assistant")
         prompt.append("")
         prompt_str = "\n".join(prompt)
-
-
-
-        # Verifica se o usuário deseja se cadastrar
-        if "quero ser parceiro" in prompt_str.lower() or "desejo criar uma conta de parceiro" in prompt_str.lower() or "conta de parceiro" in prompt_str.lower() or "quero me tornar parceiro" in prompt_str.lower() or "como faço para ser parceiro" in prompt_str.lower() or "quero ter uma conta de parceiro" in prompt_str.lower() or "quero ser um parceiro" in prompt_str.lower():
-            st.write("Para se tornar um parceiro na ORÁCULO IA e começar a ter ganhos extraordinários clique no botão abaixo.")
-            if st.button("QUERO SER PARCEIRO"):
-                showSbconta()
-                st.stop()
-
-
-        elif get_num_tokens(prompt_str) >= 8000:  # padrão3072
-            st.error(
-                "Poxa, você já atingiu seu limite de demostração, mas pode ficar tranquilo. Clique no botão abaixo para "
-                "pedir seu acesso.")
-            st.button('PEDIR ACESSO', on_click=clear_chat_history, key="clear_chat_history")
-            excel_bytes = create_excel(st.session_state.messages)
-            pdf_bytes = create_pdf(st.session_state.messages)
-            formato_arquivo = st.selectbox("Escolha como deseja baixar sua conversa:", ["PDF", "Excel"])
-            if formato_arquivo == "PDF":
-                st.download_button(
-                    label="Baixar PDF",
-                    data=pdf_bytes,
-                    file_name="conversa.pdf",
-                    mime="application/pdf",
-                )
-            else:
-                st.download_button(
-                    label="Baixar Excel",
-                    data=excel_bytes,
-                    file_name="conversa.xlsx",
-                    mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-                )
-            st.stop()
+            
 
 
         for event in replicate.stream(
@@ -467,7 +360,7 @@ def showMestre():
                     "temperature": 0.1,
                     "system_prompt": system_prompt,
                     "length_penalty": 1,
-                    "max_new_tokens": 8000,
+                    "max_new_tokens": 12000,
                 },
         ):
             yield str(event)
